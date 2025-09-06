cc1e89e6fde692a5f8ebeef7e580e8e70ae90886
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-06 10:10:15 +0100

    used string formatter to pass in average_age

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -31,7 +31,8 @@ def calculate_mean_age():
     for age in stream_user_ages():
         iteration += 1
         age_sum += age
-    print(f'Average age of users: {age_sum / iteration}') 
+    average_age = age_sum / iteration
+    print(f'Average age of users: {average_age}') 
 
 calculate_mean_age()
 
