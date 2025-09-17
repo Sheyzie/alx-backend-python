8a34434180625c65de49c5f00fb05951f4bb9fa6
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-17 18:23:31 +0100

    reduced blank lines

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,8 +5,6 @@ from unittest.mock import patch
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
-
-
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand(
         [
