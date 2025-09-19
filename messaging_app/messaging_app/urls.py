8865ffe2cf2983faac4cf1067c115a8ef90f8753
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-19 15:43:46 +0100

    add add conversation and message views and url path

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from chats.views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')  # Optional
 
 
 BASE_API = 'api/v1'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(f'{BASE_API}/', include('chats.urls')),
+    path(f'{BASE_API}/', include(router.urls)),
 ]
