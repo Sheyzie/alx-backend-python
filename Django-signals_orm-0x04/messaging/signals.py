18b19cbb466910f3c4b1ca9daf5dff7b83cbde5a
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-30 14:30:27 +0100

    Implement Signals for User Notifications

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,9 +1,16 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
-from .models import Message
-from .utils import create_message_notification, log_to_message_history
+from .models import Message, Notification, MessageHistory
+# from .utils import create_message_notification, log_to_message_history
 
+
+def create_message_notification(message):
+    Notification.objects.create(message=message, receiver=message.receiver)
+
+def log_to_message_history(message):
+    if message.edited == True:
+        message_history = MessageHistory.objects.create(message=message, message_body=message.message_body)
 
 @receiver(post_save, sender=Message)
 def create_notification_on_created_message(sender, instance, created, **kwargs):
